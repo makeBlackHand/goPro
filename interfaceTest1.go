bb15c480d5e05666f54b8f6cca2b6c84472390a5
makeBlackHand <09230612@nnutc.edu.cn>
2024-04-14 00:11:01 +0800

    更改

---
+++ interfaceTest1.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
+
+type Students struct {
+	Name  string
+	Age   int
+	Score float64
+}
+type StudentSlice []Students
+
+func (s StudentSlice) Len() int {
+	return len(s)
+}
+func (s StudentSlice) Less(i, j int) bool {
+	return s[i].Score < s[j].Score
+}
+func (s StudentSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func main() {
+	var s StudentSlice
+	for i := 0; i < 5; i++ {
+		s1 := Students{
+			Name:  fmt.Sprintf("学生-%d", rand.Intn(10)),
+			Age:   rand.Intn(20),
+			Score: float64(rand.Intn(100)) + rand.Float64(),
+		}
+		s = append(s, s1)
+	}
+	sort.Sort(s)
+	fmt.Println(s)
+
+}
