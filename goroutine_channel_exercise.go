d50b8c48de10dd0c88416899324e81c5ae94ff48
makeBlackHand <09230612@nnutc.edu.cn>
2024-05-09 14:59:21 +0800

    Merge remote-tracking branch 'origin/master'

--- goroutine_channel_exercise.go
+++ goroutine_channel_exercise.go
@@ -1,58 +1,50 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func writes(numChan chan int) {
-	for i := 1; i <= 10; i++ {
-		//time.Sleep(time.Second)
-		//fmt.Println("传数据")
-		numChan <- i
+func write100(intChan chan int) {
+	for i := range 100 {
+		intChan <- i
 	}
-	close(numChan)
+	close(intChan)
+	fmt.Println("Finished.")
 }
 
-func reads(s int, numChan chan int, resChan chan int, exitChan chan bool) {
-	fmt.Println("goroutine ", s)
+func fib64(n int) int {
 	var sum int
-	numChanLen := cap(numChan)
-	for a := 0; a < numChanLen; a++ {
-		n, ok := <-numChan
-		fmt.Println("n", n, ok)
-		if ok {
-			sum = 0
-			for i := 1; i <= n; i++ {
-				sum += i
-			}
-			//time.Sleep(time.Second)
-			//fmt.Println("写数据-")
-			resChan <- sum
-		}
+	for i := 1; i <= n; i++ {
+		sum += i
 	}
+	return sum
+}
 
+func procecss100(s int, intChan chan int, resChan chan int, exitChan chan bool) {
+	fmt.Println("Process ", s)
+	numChan := len(intChan)
+	for i := 0; i < numChan; i++ {
+		resChan <- fib64(<-intChan)
+	}
 	exitChan <- true
 }
 
 func main() {
-	numChan := make(chan int, 10)
-	resChan := make(chan int, 10)
-	flagChan := make(chan bool, 1)
-	writes(numChan)
+	intChan := make(chan int, 100)
+	resChan := make(chan int, 100)
+	endChan := make(chan bool, 4)
 
-	for i := 0; i < 1; i++ {
-		//time.Sleep(time.Second)
-		//fmt.Println("read", i)
-		go reads(i, numChan, resChan, flagChan)
+	write100(intChan)
+
+	for i := 0; i < 4; i++ {
+		go procecss100(i, intChan, resChan, endChan)
 	}
 
-	for i := 0; i < 1; i++ {
-		<-flagChan
+	for i := 0; i < 4; i++ {
+		<-endChan
 	}
-	close(flagChan)
+	close(endChan)
 	close(resChan)
 
-	for i := range resChan {
-		fmt.Println(i)
+	for i := 0; i < 100; i++ {
+		println(<-resChan)
 	}
 }
